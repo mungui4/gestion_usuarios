985b163be89fb7aff64b75e99c6cc92bb205bc3b
Rals <raulgallardo6@gmail.com>
2025-01-26 16:31:22 -0600

    ✨ add validation of roles in endpoints

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 
 class UserController extends Controller
@@ -74,7 +75,7 @@ class UserController extends Controller
         }
     }
 
-    public function logout(Request $request) //Cierra sesió eliminando los tokens de acceso
+    public function logout(Request $request) //Cierra sesión eliminando los tokens de acceso
     {
 
         try {
@@ -91,7 +92,32 @@ class UserController extends Controller
         };
     }
 
-    public function index() //Muestra la información del usuario
+    public function index(){ //Muestra todos los usuarios registrados
+        
+        // Verificar si el usuario tiene permisos para realizar esta acción
+        if (Auth::user()->role != 'admin') {
+            return response()->json([
+                'message' => 'No tienes permisos para realizar esta acción'
+            ], 401);
+        }
+        
+        $users = User::all();
+        return response()->json([
+            'message' => 'Usuarios registrados',
+            'data' => $users
+        ], 200);
+    }
+
+    public function allNormalUsers(){ //Muestra todos los usuarios con rol "user"
+        
+        $users = User::where('role', 'user')->get();
+        return response()->json([
+            'message' => 'Usuarios registrados',
+            'data' => $users
+        ], 200);
+    }
+
+    public function perfil() //Muestra la información del usuario
     {
         //
         $usuario = Auth::user();
@@ -131,4 +157,83 @@ class UserController extends Controller
             ], 400);
         }
     }
+
+    public function destroy($id) //Elimina un usuario
+    {
+        // Verificar si el usuario tiene permisos para realizar esta acción
+        if (Auth::user()->role != 'admin') {
+            return response()->json([
+                'message' => 'No tienes permisos para realizar esta acción'
+            ], 401);
+        }
+
+        $perfil = User::findOrfail($id);
+            // Verificar si el usuario a eliminar es un administrador
+        if ($perfil->role == 'admin') {
+            return response()->json([
+                'message' => 'No puedes eliminar a otro administrador'
+            ], 403);
+        }
+
+        $perfil->delete();
+        return response()->json([
+            'message' => 'Usuario eliminado'
+        ], 200);
+    }
+
+    //función que retorna los usuarios registrados en un dia específico
+    public function usersByDate(Request $request)
+    {
+        // Verificar si el usuario tiene permisos para realizar esta acción
+        if (Auth::user()->role != 'admin') {
+            return response()->json([
+                'message' => 'No tienes permisos para realizar esta acción'
+            ], 401);
+        }
+
+        $date = $request->query('date');
+
+        //Validar formato de fecha
+        if(!Carbon::hasFormat($date, 'Y-m-d')){
+            return response()->json([
+                'message' => 'Formato de fecha incorrecto'
+            ], 400);
+        }
+        $carbonDate = Carbon::createFromFormat('Y-m-d', $date);
+
+        $users = User::whereDate('created_at', $carbonDate)->get();
+        return response()->json([
+            'message' => 'Usuarios registrados en la fecha: ' . $date,
+            'data' => $users
+        ], 200);
+    }
+
+    public function usersByDates(Request $request)
+    {
+        // Verificar si el usuario tiene permisos para realizar esta acción
+        if (Auth::user()->role != 'admin') {
+            return response()->json([
+                'message' => 'No tienes permisos para realizar esta acción'
+            ], 401);
+        }
+
+        $date1 = $request->query('date1');
+        $date2 = $request->query('date2');
+
+        //Validar formato de fecha
+        if(!Carbon::hasFormat($date1, 'Y-m-d') || !Carbon::hasFormat($date2, 'Y-m-d')){
+            return response()->json([
+                'message' => 'Formato de fecha incorrecto'
+            ], 400);
+        }
+        $carbonDate1 = Carbon::createFromFormat('Y-m-d', $date1);
+        $carbonDate2 = Carbon::createFromFormat('Y-m-d', $date2);
+
+        $users = User::whereBetween('created_at', [$carbonDate1, $carbonDate2])->get();
+        return response()->json([
+            'message' => 'Usuarios registrados entre las fechas: ' . $date1 . ' y ' . $date2,
+            'data' => $users
+        ], 200);
+    }
+
 }
